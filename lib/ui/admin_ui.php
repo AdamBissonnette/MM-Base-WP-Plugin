f6b9713503d49d03c5aa930079589a7311bf4e6f
Adam B <gadimus@gmail.com>
2014-09-23 16:00:48 -0600

    Fixed tabpane bug
    A div wasn't closed causing the plugin / admin tabs to not hide show
    properly.

--- lib/ui/admin_ui.php
+++ lib/ui/admin_ui.php
@@ -14,8 +14,8 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<ul class="nav nav-pills">
-					<li class="active"><a href="#plugin" data-toggle="tab">Plugin Stuff</a></li>
-					<li><a href="#admin" data-toggle="tab">Admin</a></li>
+					<li class="active"><a href="#plugin" data-toggle="tab">Bingo Cards</a></li>
+					<li><a href="#admin" data-toggle="tab">Default Options</a></li>
 				</ul>
 				<div class="row tab-content">
 					<div class="tab-pane active" id="plugin">
@@ -23,22 +23,23 @@
 							echo MmmToolsNamespace\OutputThemeData($mmm_plugin_data, null, $Mmm_Bingo);
 						?>
 					</div>
-					<div class="tab-pane" id="admin">
-						<form id="theme_settings" onsubmit="javascript: SaveOptions(this);" class="form-horizontal" method="post">
-							<?php							
-								echo MmmToolsNamespace\OutputThemeData($mmm_bingo_options, null, $Mmm_Bingo);
-							?>
-							
-							<div class="form-controls">
-								<div class="col-sm-12">
-									<div class="form-actions clearfix">
-										<a href="#" id="btnOptionsSave" name="mmm_settings_saved" class="btn btn-primary">Save</a>
-										<input type="reset" class="btn" />
-									</div>
+				</div>
+				<div class="tab-pane" id="admin">
+					<form id="theme_settings" onsubmit="javascript: SaveOptions(this);" class="form-horizontal" method="post">
+						<?php							
+							echo MmmToolsNamespace\OutputThemeData($mmm_bingo_options, null, $Mmm_Bingo);
+						?>
+						
+						<div class="form-controls">
+							<div class="col-sm-12">
+								<div class="form-actions clearfix">
+									<a href="#" id="btnOptionsSave" name="mmm_settings_saved" class="btn btn-primary">Save</a>
+									<input type="reset" class="btn" />
 								</div>
 							</div>
-						</form>
-					</div>
+						</div>
+					</form>
+				</div>
 				</div>
 			</div>
 		</div>
