cb10622a65e14bcfcb7559071850640af129b783
Adam B <gadimus@gmail.com>
2014-09-23 10:19:53 -0600

    v0.2 update
    Added table output, reworked how the file array is built and cleaned up
    some of the to be more consistent.

--- mm_filelist_plugin.php
+++ mm_filelist_plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Mmm Simple File List
 Plugin URI: http://www.mediamanifesto.com
 Description: Plugin to list files in a given directory using this shortcode [MMFileList folder="optional starting from base uploads path" format="li (html) or comma (txt)"" types="optional file-extension e.g. pdf,doc" class="optional css class for html list"]
-Version: 0.1a
+Version: 0.3
 Author: Adam Bissonnette
 Author URI: http://www.mediamanifesto.com
 */
@@ -44,7 +44,7 @@ class MM_FileList
     			{		 
     				if(!is_dir($dir.'/'.$file))
     				{
-    					$list[$file] = $outputDir . '/' . $file;
+    					array_push($list, array("name" => $file, "url" => $outputDir . "/" . $file, "size" => $this->human_filesize(filesize($dir . '/' . $file))));
     				} 
     			}
             }
@@ -53,33 +53,65 @@ class MM_FileList
         $output = "";
         
         switch($format){
-        	case 'li':
-        		return $this->_MakeHtmlList($list, $class);
-        	break;
+            case 'table':
+                return $this->_MakeTabularLIst($list, $class);
+            break;
         	case 'comma':
-        	default:
-        		$output = implode(",", $list);
+        	    $output = $this->_MakeCommaDelimitedList($list);
  			break;
+            case 'li':
+            default:
+                return $this->_MakeUnorderedList($list, $class);
+            break;
         }
         
         return $output;
     }
 
-	function _MakeHtmlList($list, $class)
+    function _MakeCommaDelimitedList($list)
+    {
+        $formattedList = array_map(function($entry) { $listItemTemplate = "%s"; return sprintf($listItemTemplate, $entry["url"]); },$list);
+
+        return implode(",", $formattedList);
+    }
+
+	function _MakeUnorderedList($list, $class)
 	{
 		//These templates could be set as editable / saveable options
 		$listTemplate = '<ul class="' . $class . '">%s</ul>';
-		$listItemTemplate = '<li><a href="%s">%s</a></li>';
+		$listItemTemplate = '<li><a href="%s"><span class="filename">%s</span><span class="filesize"> (%s)</span></a></li>';
 		
 		$items = "";
 		
-		foreach ($list as $item => $value) //in this case item == filename, value == path
+		foreach ($list as $file => $fileatts) //in this case item == filename, value == path
 		{
-			$items .= sprintf($listItemTemplate, $value, $item);
+			$items .= sprintf($listItemTemplate, $fileatts["url"], $fileatts["name"], $fileatts["size"]);
 		}
 		
 		return sprintf($listTemplate, $items);
 	}
+
+    function _MakeTabularList($list, $class)
+    {
+        $listTemplate = '<table class="%s">%s%s</table>';
+        $listHeadingTemplate = '<tr><th class="filename">Filename / Link</th><th class="filesize">Size</th></tr>';
+        $listItemTemplate = '<tr><td class="filename"><a href="%s">%s</a></td><td class="filesize">%s</td></tr>';
+
+        $items = "";
+
+        foreach ($list as $filename => $fileatts) {
+            $items .= sprintf($listItemTemplate, $fileatts["url"], $fileatts["name"], $fileatts["size"]);
+        }
+
+        return sprintf($listTemplate, $class, $listHeadingTemplate, $items);
+    }
+
+    //Stolen from comments : http://php.net/manual/en/function.filesize.php thx Rommelsantor.com
+    function human_filesize($bytes, $decimals = 2) {
+      $sz = 'BKMGTP';
+      $factor = floor((strlen($bytes) - 1) / 3);
+      return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+    }
 
 } // end class
 
