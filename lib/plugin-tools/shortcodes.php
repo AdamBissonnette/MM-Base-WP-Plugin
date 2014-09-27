a1dd5e0fbfd4f895122c9b2472213e10357c0dd8
Adam Bissonnette <adam@mediamanifesto.com>
2014-09-27 14:08:42 -0600

    BingoCard output completed
    The short code has been setup with some basic error handling to output
    BingoCards via the short code.  Formatting and other sizing will need
    to be addressed but this preliminary version is solid.

--- lib/plugin-tools/shortcodes.php
+++ lib/plugin-tools/shortcodes.php
@@ -14,7 +14,6 @@ function MMBingoCard($atts)
     wp_enqueue_style('font-awesome', $admin_path . '/assets/css/font-awesome.css', false, null);
 
 
-
     extract( shortcode_atts( array(
             'id' => '0',
             'title' => '',
@@ -24,41 +23,66 @@ function MMBingoCard($atts)
 
     $content = "";
 
-    if ($title == '')
+    if ($id != 0)
     {
-        $title = $Mmm_Bingo->get_setting("default_title");
-    }
+        $card = $Mmm_Bingo->get_post_meta($id);
+        $post = get_post($id);
+    
+        if (isset($post))
+        {
+            if ($title == '')
+            {
+                $title = $post->post_title;
+            }
 
-    $Topics = sortTopics($Mmm_Bingo->get_setting("default_topics"));
-    $CenterIcon = $Mmm_Bingo->get_setting("default_star");
+            $Topics = _sortTopics($card["topics"]);
+            $CenterIcon = $card["icon"];
+        }
+        else
+        {
+            if ($title == '')
+            {
+                $title = $Mmm_Bingo->get_setting("default_title");
+            }
+
+            $Topics = _sortTopics($Mmm_Bingo->get_setting("default_topics"));
+            $CenterIcon = $Mmm_Bingo->get_setting("default_star");
+        }
+    }
 
     $cardTemplate = '<div class="mmbc_wrapper"><h4>%s</h4><table id="mmbc-%s" class="BingoCard %s">%s</table></div>';
     
-    //print_r($Topics);
-    for ($i = 0; $i < 25; $i++)
+    if (count($Topics) < 25)
     {
-        if ($i == 0)
+        return "This bingo card has less than the required number of topics to be generated.";
+    }
+    else
+    {
+        for ($i = 0; $i < 25; $i++)
         {
-            $content .= "<tr class=\"row-1\">";
-        }
-        else if ($i % 5 == 0)
-        {
-            $content .= sprintf("</tr>\n<tr class=\"row-%s\">\n", ($i / 5 + 1));
-        }
-    
-        $content .= _genCardSquare($Topics[$i], ($i % 5 == 0));
-    
-        if ($i == 11)
-        {
-            $content .= _genCenterSquare($CenterIcon);
-            $i++;
+            if ($i == 0)
+            {
+                $content .= "<tr class=\"row-1\">";
+            }
+            else if ($i % 5 == 0)
+            {
+                $content .= sprintf("</tr>\n<tr class=\"row-%s\">\n", ($i / 5 + 1));
+            }
+        
+            $content .= _genCardSquare($Topics[$i], ($i % 5 == 0));
+        
+            if ($i == 11)
+            {
+                $content .= _genCenterSquare($CenterIcon);
+                $i++;
+            }
         }
     }
 
     return sprintf($cardTemplate, $title, $id, $class, $content);
 }
 
-function sortTopics($Topics) {
+function _sortTopics($Topics) {
     $SplitArray = explode("\n", $Topics);
     
     shuffle($SplitArray);
