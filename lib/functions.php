924d202d164f5808e5bb1e4c96fd703c98a9a222
Adam Bissonnette <adam@mediamanifesto.com>
2015-06-04 15:37:26 -0600

    Removed Unused Includes

--- lib/functions.php
+++ lib/functions.php
@@ -5,16 +5,12 @@
 	include_once('tools/date-tools.php');
 	include_once('tools/email-tools.php');
 	include_once('tools/wp-tools.php');
-	include_once('tools/shortcodes.php');
-
-	include_once('data/taxonomy_data.php');
 
 	include_once('plugin-tools/setup.php');
 	include_once('plugin-tools/ajax.php');
 	include_once('plugin-tools/data-logic.php');
 	include_once('plugin-tools/business-logic.php');
 	include_once('plugin-tools/reporting-logic.php');
-	include_once('plugin-tools/calendar-hooks.php');
 	include_once('plugin-tools/shortcodes.php');
 
 	class CurlHandler
