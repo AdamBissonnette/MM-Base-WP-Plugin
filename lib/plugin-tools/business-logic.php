4e3ab311512aef674593c71928666eb093fa290e
Adam Bissonnette <adam@mediamanifesto.com>
2015-06-07 16:29:22 -0600

    Creates new user and party through curl

--- lib/plugin-tools/business-logic.php
+++ lib/plugin-tools/business-logic.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
         $curly = $MMM_Curl_Manager->curlHandler;
 
         $atts["entityName"] = "User";
-        if ($id == -1)
+        if ($atts->id == -1)
         {
             $atts["fn"] = "POST";
             $json = json_decode($curly->DoCurl($atts), "POST");
@@ -25,30 +25,64 @@ if ( ! defined( 'ABSPATH' ) ) {
         return $json;
     }
 
+    remove_all_filters ('wpcf7_before_send_mail');
+    add_action( 'wpcf7_mail_sent', '\MmmPluginToolsNamespace\cf7_posted' );
 
-    //--- Returns ---
-    // {  
-    //    "2":{  
-    //       "id":2,
-    //       "name":"Test",
-    //       "users":[  
-    //          {  
-    //             "id":1,
-    //             "name":"Test",
-    //             "email":"info@mediamanifesto.com",
-    //             "phone":"+13063704254",
-    //             "date":1433089285000
-    //          },
-    //          {  
-    //             "id":2,
-    //             "name":"Test 2",
-    //             "email":"",
-    //             "phone":"+13069999999",
-    //             "date":1433091758000
-    //          }
-    //       ]
-    //    }
-    // }
+    function cf7_posted( $contact_form ) {
+        $title = $contact_form->title();
+
+        if ( 'Get Started' == $title ) {
+
+            // array(12) {
+            //   ["_wpcf7"]=>
+            //   string(3) "198"
+            //   ["_wpcf7_version"]=>
+            //   string(3) "4.2"
+            //   ["_wpcf7_locale"]=>
+            //   string(5) "en_US"
+            //   ["_wpcf7_unit_tag"]=>
+            //   string(17) "wpcf7-f198-p64-o1"
+            //   ["_wpnonce"]=>
+            //   string(10) "3cdd6e6630"
+            //   ["party_name"]=>
+            //   string(4) "Test"
+            //   ["name"]=>
+            //   string(4) "test"
+            //   ["email"]=>
+            //   string(13) "test@test.com"
+            //   ["phone"]=>
+            //   string(11) "13063704254"
+            //   ["_wpcf7_captcha_challenge_captcha-648"]=>
+            //   string(10) "3210000978"
+            //   ["captcha-648"]=>
+            //   string(4) "knmx"
+            //   ["_wpcf7_is_ajax_call"]=>
+            //   string(1) "1"
+            // }
+
+            $data = array('id' => -1, 'name' => $_REQUEST["name"], 'email' => $_REQUEST["email"], 'phone' => $_REQUEST["phone"], 'party_name' => $_REQUEST["party_name"]);
+
+            SaveUser($data);
+        }
+    }
+
+
+//     if ( null == username_exists( $email_address ) ) {
+ 
+//     $password = wp_generate_password( 12, false );
+//     $user_id = wp_create_user( $email_address, $password, $email_address );
+ 
+//     wp_update_user(
+//         array(
+//             'ID'          =>    $user_id,
+//             'nickname'    =>    $email_address
+//         );
+//     );
+ 
+// } else {
+//     // The username already exists, so handle this accordingly...
+// }
+
     function GetParty($atts)
     {
         global $MMM_Curl_Manager;
