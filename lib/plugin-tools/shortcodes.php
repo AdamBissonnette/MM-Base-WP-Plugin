de7fa5d3d7b2b6cb2ba7d449a0b2560b5eb7c1e9
Adam Bissonnette <adam@mediamanifesto.com>
2015-06-09 13:49:45 -0600

    Updated shortcake output

--- lib/plugin-tools/shortcodes.php
+++ lib/plugin-tools/shortcodes.php
@@ -36,30 +36,32 @@ ob_start(); //Since there isn't a nice way to get this content we have to use th
 ?>
 
 <div class="registration-forms">
+<h3>Your Details</h3>
+
     <form data-toggle="validator" name="registration" action="/get-started/" method="post" id="get_started" class="start-form">
 
 <div class="form-group">
-    <label for="inputPartyName" class="control-label">Party Name (Required)</label>
-    <div class="controls"><input type="text" class="form-control" id="inputPartyName" name="registration_party_name" value="<?php echo $party_name; ?>" required></div>
-    <div class="help-block with-errors"></div>
+    <label for="inputPartyName" class="control-label">Party Name</label>
+    <div class="controls"><input type="text" class="form-control" id="inputPartyName" name="registration_party_name" value="<?php echo $party_name; ?>" placeholder="e.g. Psychic Socks" required></div>
+    <div class="help-block with-errors"><ul class="list-unstyled"><li>*Required</li></ul></div>
 </div>
 
 <div class="form-group">
-    <label for="inputName" class="control-label">Your Name (Required)</label>
-    <div class="controls"><input type="text" class="form-control" id="inputName" name="registration_name" required></div>
-    <div class="help-block with-errors"></div>
+    <label for="inputName" class="control-label">Your Name</label>
+    <div class="controls"><input type="text" class="form-control" id="inputName" name="registration_name" placeholder="e.g. John Smith" required></div>
+    <div class="help-block with-errors"><ul class="list-unstyled"><li>*Required</li></ul></div>
 </div>
 
 <div class="form-group">
-    <label for="inputEmail" class="control-label">Your Email (Required)</label>
-    <div class="controls"><input type="email" class="form-control" id="inputEmail" name="registration_email" required></div>
-    <div class="help-block with-errors"></div>
+    <label for="inputEmail" class="control-label">Your Email</label>
+    <div class="controls"><input type="email" class="form-control" id="inputEmail" name="registration_email" placeholder="e.g. JSmith@internet.com" required></div>
+    <div class="help-block with-errors"><ul class="list-unstyled"><li>*Required</li></ul></div>
 </div>
 
 <div class="form-group">
-    <label for="inputPhone" class="control-label">Your Phone (Required)</label>
-    <div class="controls"><input type="text" pattern="\+?1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?" class="form-control" id="inputPhone" name="registration_phone" required></div>
-    <div class="help-block with-errors">Enter it like this: +13069921212</div>
+    <label for="inputPhone" class="control-label">Your Phone</label>
+    <div class="controls"><input type="text" pattern="\+?1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?" class="form-control" id="inputPhone" name="registration_phone" placeholder="e.g. +13069921212" required></div>
+    <div class="help-block with-errors"><ul class="list-unstyled"><li>*Required - enter like this: +13069921212</li></ul></div>
 </div>
 
 <div class="form-group">
@@ -99,25 +101,25 @@ ob_start(); //Since there isn't a nice way to get this content we have to use th
 <div class="add-party-member-form">
     <!-- <div id="party-form-overlay"></div> -->
 
-    <h2>Your Party Members</h2>
+    <h3>Your Party Members</h3>
 
     <form data-toggle="validator" name="add-party-member" action="/get-started/" method="post" id="add_party_member" class="start-form">
         <div class="form-group">
-            <label for="inputAPName" class="control-label">Name (Required)</label>
-            <div class="controls"><input type="text" class="form-control" id="inputAPName" name="add_party_member_name" required></div>
-            <div class="help-block with-errors"></div>
+            <label for="inputAPName" class="control-label">Name</label>
+            <div class="controls"><input type="text" class="form-control" id="inputAPName" name="add_party_member_name" placeholder="e.g. John Smith" required></div>
+            <div class="help-block with-errors"><ul class="list-unstyled"><li>*Required</li></ul></div>
         </div>
 
         <div class="form-group">
-            <label for="inputAPEmail" class="control-label">Email (Required)</label>
-            <div class="controls"><input type="email" class="form-control" id="inputAPEmail" name="add_party_member_email" required></div>
-            <div class="help-block with-errors"></div>
+            <label for="inputAPEmail" class="control-label">Email</label>
+            <div class="controls"><input type="email" class="form-control" id="inputAPEmail" name="add_party_member_email" placeholder="e.g. JSmith@internet.com" required></div>
+            <div class="help-block with-errors"><ul class="list-unstyled"><li>*Required</li></ul></div>
         </div>
 
         <div class="form-group">
-            <label for="inputAPPhone" class="control-label">Phone (Required)</label>
-            <div class="controls"><input type="text" pattern="1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?" class="form-control" id="inputAPPhone" name="add_party_member_phone" required></div>
-            <div class="help-block with-errors">Enter it like this: +13069921212</div>
+            <label for="inputAPPhone" class="control-label">Phone</label>
+            <div class="controls"><input type="text" pattern="1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?" class="form-control" id="inputAPPhone" name="add_party_member_phone" placeholder="e.g. +13069921212" required></div>
+            <div class="help-block with-errors"><ul class="list-unstyled"><li>*Required - Enter like this: +13069921212</li></ul></div>
         </div>
 
         <div class="form-group">
